a961f2c40545cfe74707ef76740a6bf58dfa4fbb
Alok Kumar <akumar@saltside.se>
2017-02-16 14:29:19 +0530

    Update code for Flume data push Update code for CassandraExample

--- src/main/java/alokawi/poc/cassandra/demo/CassandraExample.java
+++ src/main/java/alokawi/poc/cassandra/demo/CassandraExample.java
@@ -3,11 +3,15 @@
  */
 package alokawi.poc.cassandra.demo;
 
+import java.util.List;
+
 import alokawi.poc.cassandra.core.CassandraConnection;
 import alokawi.poc.cassandra.core.CassandraDBContext;
-import alokawi.poc.cassandra.core.CassandraQuery;
 import alokawi.poc.cassandra.core.Connection;
 import alokawi.poc.exception.QueryExecutionException;
+import alokawi.poc.flume.example.FlumeDataWriter;
+import alokawi.poc.videoview.VideoViewEvent;
+import alokawi.poc.videoview.VideoViewEventDataGenerator;
 
 /**
  * @author alokkumar
@@ -26,9 +30,42 @@ public class CassandraExample {
 
 	private void loadDataToCassandra(String node, int port) throws QueryExecutionException {
 
-		Connection<CassandraDBContext> connection = new CassandraConnection(node, port);
+		int numberOfUsers = 1000;
+		int numberOfVideos = 100;
+
+		int numberOfRecords = 100000;
+
+		// timeRange = 1Feb to 15Feb
+		long timeOrigin = 1485907200000L;
+		long timeEnd = 1487226374000L;
+
+		// interval k = 5 here
+		int viewDurationInterval = 5;
+
+		VideoViewEventDataGenerator eventDataGenerator = new VideoViewEventDataGenerator();
+		List<VideoViewEvent> generateData = eventDataGenerator.generate(numberOfUsers, numberOfVideos, numberOfRecords,
+				timeOrigin, timeEnd, viewDurationInterval);
+
 		
-		connection.execute(new CassandraQuery("Select * from test.words"));
+		// Push data to Flume
+		FlumeDataWriter flumeDataWriter = new FlumeDataWriter();
+
+		for (VideoViewEvent videoViewEvent : generateData) {
+			System.out.println(videoViewEvent);
+
+			flumeDataWriter.sendDataToFlume(videoViewEvent);
+
+		}
+
+		// or Push directly to Cassandra
+		Connection<CassandraDBContext> connection = new CassandraConnection(node, port);
+
+		for (VideoViewEvent videoViewEvent : generateData) {
+			System.out.println(videoViewEvent);
+
+			connection.insertVideoEvent(videoViewEvent);
+		}
+
 	}
 
 }
