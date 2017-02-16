9843a3ec51c523133bad7c52f6b274e85a71a964
Alok Kumar <akumar@saltside.se>
2017-02-16 11:16:08 +0530

    Update time-dutration to [5-45]

--- src/main/java/alokawi/poc/videoview/VideoViewEventDataGenerator.java
+++ src/main/java/alokawi/poc/videoview/VideoViewEventDataGenerator.java
@@ -37,12 +37,12 @@ public class VideoViewEventDataGenerator {
 			videoViewEvent.setUserId("u-" + UUID.randomUUID().toString());
 			videoViewEvent.setVideoId("v-" + UUID.randomUUID().toString());
 			videoViewEvent.setSessionId("s-" + UUID.randomUUID().toString());
-			
-			// Randomly increase and set time 
+
+			// Randomly increase and set time
 			videoViewEvent.setEventStartTimestamp(date.getTime() + ((i + 1) * (i + 1) * (i + 1)));
 
 			// Get duration in seconds from 5 to 45 seconds
-			videoViewEvent.setViewDurationInSeconds(5 * (i % 10));
+			videoViewEvent.setViewDurationInSeconds(5 * (i % 9) + 5);
 
 			System.out.println(i + " : " + videoViewEvent);
 			videoViewEvents.add(videoViewEvent);
