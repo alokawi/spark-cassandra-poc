a523cd8059d0df736125aceabefa382a50485d51
Alok Kumar <akumar@saltside.se>
2017-02-23 20:04:38 +0530

    Add loop to calculate per day drop-off

--- src/main/java/alokawi/poc/spark/core/SparkCassandraUtils.java
+++ src/main/java/alokawi/poc/spark/core/SparkCassandraUtils.java
@@ -4,7 +4,9 @@
 package alokawi.poc.spark.core;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
@@ -51,14 +53,37 @@ public class SparkCassandraUtils {
 				.cache();
 
 		dataset.registerTempTable("temptable");
-		dataset.filter(new Column("view_duration_in_second").equalTo("15")).show();
 
-		String query = "select * from temptable where video_id = 'v-44'";
+		String query = "select video_id, view_duration_in_second, count(*) from temptable group by 1, 2";
 
-		sqlContext.sql(query).show();
+		List<Row> collectAsList = sqlContext.sql(query).collectAsList();
+		for (Row row : collectAsList) {
+			System.out.println(row.get(0) + "," + row.get(1) + "," + row.get(2));
+		}
 
-		System.out.println(sqlContext);
+		// sqlContext.sql(query).show(1000);
 
+		long startTime = 1485907200000L;
+		long endTime = 1487226374000L;
+
+		for (long i = startTime; i <= endTime; i = i + TimeUnit.DAYS.toMillis(1)) {
+
+			dataset.filter(new Column("event_start_timestamp").geq(i))
+					.filter(new Column("event_start_timestamp").leq(i + TimeUnit.DAYS.toMillis(1)))
+					.groupBy(new Column("view_duration_in_second"), new Column("video_id")).count()
+					.orderBy("view_duration_in_second").show(1000);
+			sleepDelay();
+			
+		}
+
+	}
+
+	private void sleepDelay() {
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
